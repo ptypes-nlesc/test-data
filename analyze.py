eaaf2826928a5f612d52700d6eb6ab5c230754de
Jisk Attema <j.attema@esciencecenter.nl>
2024-01-29 14:10:30 +0100

    Use sweetviz for data exploration

---
+++ analyze.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sweetviz.sv_public import analyze
+
+
+if __name__ == "__main__":
+    df = pd.read_csv('xhamster.csv.tar.gz')
+
+    analyze_report = analyze(df)
+    analyze_report.show_html('xhamster.html', open_browser=True)
+
+    df = pd.read_csv('xnxx.csv.tar.gz')
+
+    analyze_report = analyze(df)
+    analyze_report.show_html('xnxx.html', open_browser=True)
