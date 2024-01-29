b2847351feb750f3f469999d3651f5872084bc58
Olga Lyashevska <olga@herenstraat.nl>
2024-01-29 14:27:47 +0100

    add n tags

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ from sweetviz.sv_public import analyze
 
 if __name__ == "__main__":
     df = pd.read_csv('xhamster.csv.tar.gz')
+    df['n_tags'] = df['tags'].apply(len)
 
     analyze_report = analyze(df)
     analyze_report.show_html('xhamster.html', open_browser=True)
