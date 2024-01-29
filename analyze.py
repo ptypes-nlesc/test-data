120685f18b411c7fb9c73d7907530ebe791bc97e
Jisk Attema <j.attema@esciencecenter.nl>
2024-01-29 15:17:51 +0100

    BUGFIX: typo

--- analyze.py
+++ analyze.py
@@ -3,6 +3,10 @@ from sweetviz.sv_public import analyze
 from collections import Counter
 
 
+def my_split(s, counter=None):
+    counter.update(s.replace("[u", "").replace("']", "'").replace("u'", "'").split(", "))
+
+
 if __name__ == "__main__":
     df = pd.read_csv('xhamster.csv.tar.gz')
 
@@ -13,14 +17,10 @@ if __name__ == "__main__":
     df['n_tags'] = df['tags'].apply(len)
 
     counter = Counter()
-
-    def my_split(s):
-        counter.update(s.replace("[u", "").replace("']", "'").replace("u'", "'").split(", "))
-
-    df['tags'].apply(counter)
+    df['tags'].apply(my_split, counter=counter)
 
     with open('tags_counts.txt', 'w') as f:
-        for l in zip(count.keys(), count.values()):
+        for l in zip(counter.keys(), counter.values()):
             f.write("%s\t%i\n" % (l[0], l[1]))
 
     analyze_report = analyze(df)
